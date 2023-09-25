1b03656f7f3319eb4cdd3b25609d112254de5295
Sourav Chowdhury <souravchowdhury@Souravs-MacBook-Pro.local>
2023-09-25 15:13:50 +0300

    first commit

---
+++ API_Automation/conftest.py
@@ -0,0 +1,211 @@
+
+import pytest
+import requests
+from faker import Faker
+import logging
+import json
+
+fake = Faker()
+
+BASE_URL = 'https://gorest.co.in/public/v2/users/'
+ACCESS_TOKEN = ''
+
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+class JsonFormatter(logging.Formatter):
+    def format(self, record):
+        log_data = {
+            'timestamp': self.formatTime(record),
+            'level': record.levelname,
+            'message': record.getMessage(),
+            'module': record.module,
+            'filename': record.filename,
+            'lineno': record.lineno,
+        }
+        return json.dumps(log_data)
+
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+log_file_path = 'debug_logs.json'
+file_handler = logging.FileHandler(log_file_path, mode='w', encoding='utf-8')
+json_formatter = JsonFormatter()
+file_handler.setFormatter(json_formatter)
+logger.addHandler(file_handler)
+
+def get_headers():
+    return {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {ACCESS_TOKEN}',
+    }
+
+@pytest.fixture(scope="module")
+def test_data():
+    headers = get_headers()
+
+
+    data = {
+        'name': fake.name(),
+        'gender': fake.random_element(elements=('male', 'female')),
+        'email': fake.email(),
+        'status': 'active'
+    }
+    response = requests.post(BASE_URL, json=data, headers=headers)
+    assert response.status_code == 201
+    response_data = response.json()
+
+
+    user_id = response_data["id"]
+
+
+    logger.debug(f"User created with ID: {user_id}")
+    logger.debug(f"User data: {response_data}")
+
+    yield user_id, response_data
+
+@pytest.fixture(scope="function")
+def test_validate_new_user(test_data):
+    user_id, post_data = test_data
+    headers = get_headers()
+
+    response = requests.get(f'{BASE_URL}/{user_id}', headers=headers)
+    assert response.status_code == 200
+    response_data = response.json()
+
+
+    logger.debug(f"Validating user with ID: {user_id}")
+    logger.debug(f"Response data: {response_data}")
+
+
+    assert response_data["id"] == user_id
+    assert response_data["email"] == post_data["email"]
+    assert response_data["name"] == post_data["name"]
+
+    return response_data
+
+@pytest.fixture(scope="function")
+def test_updated_credentials(test_data):
+    user_id, post_data = test_data
+    headers = get_headers()
+
+    test_data = {
+        'name': fake.name(),
+        'gender': fake.random_element(elements=('male', 'female')),
+        'email': fake.email(),
+        'status': 'active'
+    }
+
+    response = requests.patch(f'{BASE_URL}/{user_id}', json=test_data, headers=headers)
+    assert response.status_code == 200
+    patch_response_data = response.json()
+    logger.debug(f"User created with ID: {user_id}")
+    logger.debug(f"User data: {patch_response_data}")
+    return patch_response_data
+
+@pytest.fixture(scope="function")
+def user_id(test_data):
+    user_id, _ = test_data
+    return user_id
+
+@pytest.fixture(scope="function")
+def test_validating_same_user_creds_updated(user_id):
+    headers = get_headers()
+    response = requests.get(f'{BASE_URL}/{user_id}', headers=headers)
+    assert response.status_code == 200
+    logger.debug(f"User created with ID: {user_id}")
+    logger.debug(f"User data: {response.json()}")
+    return response.json()
+
+@pytest.fixture(scope="function")
+def test_delete_user(user_id):
+    headers = get_headers()
+
+    response = requests.delete(f'{BASE_URL}/{user_id}', headers=headers)
+
+    assert response.status_code == 204
+    logger.debug(f"Deleting user with ID: {user_id}")
+    logger.debug(f"Response status code: {response.status_code}")
+    logger.debug(f"Response data: {response.text}")
+    return response
+
+@pytest.fixture(scope="function")
+def test_verify_deleted_user(user_id):
+    headers = get_headers()
+
+    response = requests.get(f'{BASE_URL}/{user_id}', headers=headers)
+
+    logger.debug(f"Verifying user with ID: {user_id}")
+    logger.debug(f"Response status code: {response.status_code}")
+    logger.debug(f"Response data: {response.text}")
+
+    if response.status_code == 404:
+        response_data = response.json()
+        assert response_data.get("message") == "Resource not found"
+        return response
+
+
+    logger.debug(f"User exists with ID: {user_id}, proceeding with DELETE request")
+
+    delete_response = requests.delete(f'{BASE_URL}/{user_id}', headers=headers)
+
+    assert delete_response.status_code == 204
+    logger.debug(f"Deleting user with ID: {user_id}")
+    logger.debug(f"DELETE Response status code: {delete_response.status_code}")
+    logger.debug(f"DELETE Response data: {delete_response.text}")
+
+    return delete_response
+
+@pytest.fixture(scope="function")
+def test_create_todo(user_id):
+    headers = get_headers()
+
+    todo_data = {
+        "title": fake.sentence(),
+        "status": fake.random_element(elements=("pending", "completed"))
+    }
+    response = requests.post(f'{BASE_URL}{user_id}/todos', json=todo_data, headers=headers)
+
+    assert response.status_code == 201
+    logger.debug(f"Verifying deleted user with ID: {user_id}")
+    logger.debug(f"Response status code: {response.status_code}")
+    logger.debug(f"Response data: {response.text}")
+
+    return response
+
+@pytest.fixture(scope="function")
+def test_get_user_todos(user_id):
+    headers = get_headers()
+
+    response = requests.get(f'{BASE_URL}/{user_id}/todos', headers=headers)
+
+    assert response.status_code == 200
+    logger.debug(f"Getting user todos for ID: {user_id}")
+    logger.debug(f"Response status code: {response.status_code}")
+    logger.debug(f"Response data: {response.text}")
+
+    return response.json()
+
+@pytest.fixture(scope="function")
+def test_patch_invalid_email(test_data):
+    user_id, _ = test_data
+    headers = get_headers()
+
+
+    invalid_test_data = {
+        'email': 'updateemail.in',
+    }
+
+    response = requests.patch(f'{BASE_URL}/{user_id}', json=invalid_test_data, headers=headers)
+
+    assert not str(response.status_code).startswith('2')
+
+    logger.debug(f"Invalid PATCH request for user ID: {user_id}")
+    logger.debug(f"Response status code: {response.status_code}")
+    logger.debug(f"Response data: {response.text}")
+
+    return response
+
---
+++ API_Automation/test_API_automation.py
@@ -0,0 +1,76 @@
+import pytest
+import json
+import logging
+from conftest import test_updated_credentials, test_validate_new_user, test_validating_same_user_creds_updated,test_patch_invalid_email
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger()
+
+
+@pytest.mark.create_user(scope="function")
+@pytest.mark.description("Scenario: CRUD Operation for https://gorest.co.in/\n"
+                         "Steps:\n"
+                         "1) Create new USER (via API)\n"
+                         "2) Verify if USER is created\n"
+                         "3) Create ToDo for user\n"
+                         "4) Verify ToDo is created as intended\n"
+                         "5) Update USER credentials\n"
+                         "6) Verify USER Credentials are updated correctly USER\n"
+                         "7) Delete the USER\n"
+                         "8) Verify correct USER is deleted"
+                         "9) Negative-scenario try to update USER with INVALID email format"
+                         )
+def test_create_new_user(test_data):
+    logger.info("User Data:")
+    logger.info(json.dumps(test_data, indent=4))
+
+    assert test_data is not None
+
+
+def test_confirm_the_new_user_created(test_data, test_validate_new_user):
+    response_data = test_validate_new_user
+
+    assert response_data["status"] == "active"
+    assert response_data["name"] is not None
+    assert response_data["email"] is not None
+    assert response_data["gender"] is not None
+    return response_data
+
+def test_create_todo_for_user(test_create_todo, test_data):
+    user_id, _ = test_data
+
+def test_confirm_user_todos_created(test_create_todo, test_get_user_todos):
+    post_response = test_create_todo
+    get_response = test_get_user_todos
+
+def test_update_user_credentials(test_updated_credentials):
+    updated_data = test_updated_credentials
+    assert updated_data is not None
+
+
+def test_confirm_users_credentials_updated(test_validating_same_user_creds_updated):
+    get_response_data = test_validating_same_user_creds_updated
+
+    expected_name = get_response_data["name"]
+    expected_email = get_response_data["email"]
+    expected_gender = get_response_data["gender"]
+
+    assert get_response_data["name"] == expected_name
+    assert get_response_data["email"] == expected_email
+    assert get_response_data["gender"] == expected_gender
+
+
+def test_delete_user(test_data, test_delete_user):
+    user_id, _ = test_data
+
+
+def test_verify_deleted_user(test_data, test_verify_deleted_user):
+    user_id, _ = test_data
+
+@pytest.mark.usefixtures("test_patch_invalid_email")
+def test_negative_invalid_patch_request(test_patch_invalid_email):
+    assert not str(test_patch_invalid_email.status_code).startswith('2')
+
+
+
+
